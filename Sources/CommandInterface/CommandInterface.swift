64cf69669c17c4896ddd21547fa226dfefed7eba
Vaida <91354917+Vaida12345@users.noreply.github.com>
2023-02-12 03:56:34 +1100

    Initial Commit

---
+++ Sources/CommandInterface/CommandInterface.swift
@@ -0,0 +1,6 @@
+public struct CommandInterface {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/CommandInterfaceTests/CommandInterfaceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import CommandInterface
+
+final class CommandInterfaceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CommandInterface().text, "Hello, World!")
+    }
+}
